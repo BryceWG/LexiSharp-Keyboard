823f1a6e92beeec7065ec62abef15c6b493c71cf
Luolin Zhang <zhang1577006721@qq.com>
2025-10-11 16:13:30 +0800

    build(app): 提升应用版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.brycewg.asrkb"
         minSdk =31
         targetSdk = 34
-        versionCode = 18
-        versionName = "2.1.0"
+        versionCode = 19
+        versionName = "2.1.1"
     }
 
     buildTypes {
